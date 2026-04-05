41449d058e0900ac0daf80ce1c084baaa6a53983
TharushaRashmina <tharusharashmina09@gmail.com>
2026-04-05 15:22:17 +0530

    add a config file for storage

---
+++ backend/src/main/java/com/example/smart_campus_operations/config/StorageProperties.java
@@ -0,0 +1,12 @@
+package com.example.smart_campus_operations.config;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@Getter
+@Setter
+@ConfigurationProperties(prefix = "app.storage")
+public class StorageProperties {
+    private String uploadDir = "uploads";
+}
