298499c482b08e6e56ddcc82d99125fbb6f7e18a
Navindu <kndilhara01@gmail.com>
2026-04-18 20:54:54 +0530

    Resolve google login issue

--- frontend/src/pages/AdminBookingPage.js
+++ frontend/src/pages/AdminBookingPage.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
 import { toast } from 'react-toastify';
 import bookingService from '../services/bookingService';
 
